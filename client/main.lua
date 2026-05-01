afc0e7818a1d2e2278b8b7a968c169527209ba6f
Hudhali <52822865+A-A-J@users.noreply.github.com>
2026-05-01 07:40:09 +0300

    Add real NUI client callbacks

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,48 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterCommand('mdt', function()
+local function IsPolice()
     local Player = QBCore.Functions.GetPlayerData()
-    if Player and Config.PoliceJobs[Player.job.name] then
+    return Player and Player.job and Config.PoliceJobs[Player.job.name]
+end
+
+RegisterCommand('mdt', function()
+    if IsPolice() then
         SetNuiFocus(true, true)
         SendNUIMessage({ action = 'open' })
     end
 end)
 
-RegisterNUICallback('close', function()
+RegisterNUICallback('close', function(_, cb)
     SetNuiFocus(false, false)
+    cb({ ok = true })
+end)
+
+RegisterNUICallback('getData', function(_, cb)
+    QBCore.Functions.TriggerCallback('aj_mdt:getAllData', function(data)
+        cb(data)
+    end)
+end)
+
+RegisterNUICallback('addCase', function(data, cb)
+    TriggerServerEvent('aj_mdt:addCase', data)
+    Wait(250)
+    QBCore.Functions.TriggerCallback('aj_mdt:getAllData', function(result)
+        cb(result)
+    end)
+end)
+
+RegisterNUICallback('addWanted', function(data, cb)
+    TriggerServerEvent('aj_mdt:addWanted', data)
+    Wait(250)
+    QBCore.Functions.TriggerCallback('aj_mdt:getAllData', function(result)
+        cb(result)
+    end)
+end)
+
+RegisterNUICallback('addVehicleFlag', function(data, cb)
+    TriggerServerEvent('aj_mdt:addVehicle', data)
+    Wait(250)
+    QBCore.Functions.TriggerCallback('aj_mdt:getAllData', function(result)
+        cb(result)
+    end)
 end)

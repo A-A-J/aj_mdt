f6977d56463f641f7539e2dfd92b37b267bb08e9
Hudhali <52822865+A-A-J@users.noreply.github.com>
2026-05-01 07:34:54 +0300

    Add FiveM resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'aj_mdt'
+author 'A-A-J / ChatGPT'
+description 'QBCore Arabic/English MDT NUI for FiveM'
+version '0.1.0'
+
+lua54 'yes'
+
+ui_page 'web/index.html'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+files {
+    'web/index.html',
+    'web/style.css',
+    'web/app.js'
+}
